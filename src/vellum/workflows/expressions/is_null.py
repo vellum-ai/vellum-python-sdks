3eaa165d52cc091e1b61e3f3640024a35be5f51b
David Vargas <dvargas92495@gmail.com>
2024-12-20 09:48:15 -0500

    Introduce is_nil() expression

--- src/vellum/workflows/expressions/is_null.py
+++ src/vellum/workflows/expressions/is_null.py
@@ -1,6 +1,5 @@
 from typing import Generic, TypeVar, Union
 
-from vellum.workflows.constants import UNDEF
 from vellum.workflows.descriptors.base import BaseDescriptor
 from vellum.workflows.descriptors.utils import resolve_value
 from vellum.workflows.state.base import BaseState
@@ -14,9 +13,9 @@ class IsNullExpression(BaseDescriptor[bool], Generic[_T]):
         *,
         expression: Union[BaseDescriptor[_T], _T],
     ) -> None:
-        super().__init__(name=f"{expression} is None", types=(bool,))
+        super().__init__(name=f"{expression} is null", types=(bool,))
         self._expression = expression
 
     def resolve(self, state: "BaseState") -> bool:
         expression = resolve_value(self._expression, state)
-        return expression is None or expression is UNDEF
+        return expression is None
