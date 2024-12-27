804fd09c371ea4cc89bd2ab40f29eee07a3c0946
David Vargas <dvargas92495@gmail.com>
2024-12-27 12:22:51 -0500

    lint

--- ee/codegen/src/generators/base-persisted-file.ts
+++ ee/codegen/src/generators/base-persisted-file.ts
@@ -1,4 +1,4 @@
-import { mkdir, writeFile, readFile } from "fs/promises";
+import { mkdir, writeFile } from "fs/promises";
 import path, { join } from "path";
 
 import { python } from "@fern-api/python-ast";
