7ba34a1cfaae0c85342206f6f14f528939a6d50a
Pei Li <peihongli94@gmail.com>
2025-01-03 11:24:38 -0500

    update existing tests

--- ee/vellum_ee/workflows/display/nodes/vellum/base_node.py
+++ ee/vellum_ee/workflows/display/nodes/vellum/base_node.py
@@ -1,14 +1,10 @@
 from uuid import UUID
-from typing import Any, ClassVar, Dict, Generic, Optional, TypeVar, cast
+from typing import Any, Generic, Optional, TypeVar
 
 from vellum.workflows.nodes.bases.base import BaseNode
-from vellum.workflows.references.output import OutputReference
-from vellum.workflows.types.core import JsonArray, JsonObject
+from vellum.workflows.types.core import JsonObject
 from vellum_ee.workflows.display.nodes.base_node_vellum_display import BaseNodeVellumDisplay
-from vellum_ee.workflows.display.nodes.utils import raise_if_descriptor
-from vellum_ee.workflows.display.nodes.vellum.utils import create_node_input
 from vellum_ee.workflows.display.types import WorkflowDisplayContext
-from vellum_ee.workflows.display.vellum import WorkspaceSecretPointer
 
 _BaseNodeType = TypeVar("_BaseNodeType", bound=BaseNode)
 
@@ -17,7 +13,6 @@ class BaseNodeDisplay(BaseNodeVellumDisplay[_BaseNodeType], Generic[_BaseNodeTyp
     def serialize(
         self, display_context: WorkflowDisplayContext, error_output_id: Optional[UUID] = None, **kwargs: Any
     ) -> JsonObject:
-        node = self._node
         node_id = self.node_id
 
         return {
