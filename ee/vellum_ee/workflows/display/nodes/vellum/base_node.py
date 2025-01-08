b2c21e1bc8eef3fc6c6823bc198ffab2a3a31c87
Pei Li <peihongli94@gmail.com>
2025-01-08 11:51:16 -0500

    add trigger

--- ee/vellum_ee/workflows/display/nodes/vellum/base_node.py
+++ ee/vellum_ee/workflows/display/nodes/vellum/base_node.py
@@ -2,6 +2,7 @@ from typing import Any, Generic, TypeVar
 
 from vellum.workflows.nodes.bases.base import BaseNode
 from vellum.workflows.types.core import JsonObject
+from vellum.workflows.utils.uuids import uuid4_from_hash
 from vellum_ee.workflows.display.nodes.base_node_vellum_display import BaseNodeVellumDisplay
 from vellum_ee.workflows.display.types import WorkflowDisplayContext
 
@@ -10,9 +11,17 @@ _BaseNodeType = TypeVar("_BaseNodeType", bound=BaseNode)
 
 class BaseNodeDisplay(BaseNodeVellumDisplay[_BaseNodeType], Generic[_BaseNodeType]):
     def serialize(self, display_context: WorkflowDisplayContext, **kwargs: Any) -> JsonObject:
+        node = self._node
         node_id = self.node_id
 
         return {
             "id": str(node_id),
             "type": "GENERIC",
+            "display_data": self.get_display_data().dict(),
+            "definition": self.get_definition().dict(),
+            "ports": [],
+            "trigger": {
+                "id": str(uuid4_from_hash(f"{node_id}|trigger")),
+                "merge_behavior": node.Trigger.merge_behavior.value,
+            },
         }
