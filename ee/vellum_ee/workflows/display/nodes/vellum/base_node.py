27d9ef55ac4055adb9a88138fc2075215c5379e2
Pei Li <peihongli94@gmail.com>
2025-01-08 15:19:15 -0500

    fix import

--- ee/vellum_ee/workflows/display/nodes/vellum/base_node.py
+++ ee/vellum_ee/workflows/display/nodes/vellum/base_node.py
@@ -5,8 +5,7 @@ from vellum.workflows.types.core import JsonObject
 from vellum.workflows.utils.uuids import uuid4_from_hash
 from vellum_ee.workflows.display.nodes.base_node_vellum_display import BaseNodeVellumDisplay
 from vellum_ee.workflows.display.types import WorkflowDisplayContext
-
-from ee.vellum_ee.workflows.display.vellum import GenericNodeDisplayData
+from vellum_ee.workflows.display.vellum import GenericNodeDisplayData
 
 _BaseNodeType = TypeVar("_BaseNodeType", bound=BaseNode)
 
