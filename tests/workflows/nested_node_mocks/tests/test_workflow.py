9dc7f556bca693fca11ece68616913d51d209a1a
David Vargas <dvargas92495@gmail.com>
2025-01-11 15:34:06 -0500

    Explore pytest-esque patterns

--- tests/workflows/nested_node_mocks/tests/test_workflow.py
+++ tests/workflows/nested_node_mocks/tests/test_workflow.py
@@ -1,29 +1,18 @@
-from vellum.workflows.nodes.mocks import MockNodeExecution
-
-from tests.workflows.nested_node_mocks.workflow import MapFruitNode, NestedNodeMockWorkflow, NodeToMock
+from tests.workflows.nested_node_mocks.workflow import NestedNodeMockWorkflow, NodeToMock
 
 
 def test_run_workflow__happy_path():
     # GIVEN a workflow that contains a MapNode that contains the actual node we want to mock
     workflow = NestedNodeMockWorkflow()
 
-    # WHEN the workflow is run with nested mocks
-    terminal_event = workflow.run(
-        node_output_mocks=[
-            MockNodeExecution(
-                when_condition=MapFruitNode.SubworkflowInputs.index.equals(0),
-                then_outputs=NodeToMock.Outputs(result="date"),
-            ),
-            MockNodeExecution(
-                when_condition=MapFruitNode.SubworkflowInputs.index.equals(1),
-                then_outputs=NodeToMock.Outputs(result="eggplant"),
-            ),
-            MockNodeExecution(
-                when_condition=MapFruitNode.SubworkflowInputs.index.equals(2),
-                then_outputs=NodeToMock.Outputs(result="fig"),
-            ),
-        ]
+    NodeToMock.Outputs._mock_side_effect = lambda inputs: (
+        NodeToMock.Outputs(result="date")
+        if inputs.index == 0
+        else NodeToMock.Outputs(result="eggplant") if inputs.index == 1 else NodeToMock.Outputs(result="fig")
     )
+
+    # WHEN the workflow is run with nested mocks
+    terminal_event = workflow.run()
 
     # THEN the workflow should complete successfully
     assert terminal_event.name == "workflow.execution.fulfilled", terminal_event
