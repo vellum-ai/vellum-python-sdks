522b0b0468aba4c32c8740d2aa1dde6e4434b592
David Vargas <dvargas92495@gmail.com>
2025-01-28 11:19:40 -0500

    Remove new id test for now

--- ee/vellum_cli/tests/test_push.py
+++ ee/vellum_cli/tests/test_push.py
@@ -135,52 +135,6 @@ def test_push__happy_path(mock_module, vellum_client, base_command):
     assert extracted_files["workflow.py"] == workflow_py_file_content
 
 
-@pytest.mark.skip(
-    reason="""Open question on whether we should allow this. \
-We should first consider whether we want to enable pushing without config"""
-)
-def test_push__workflow_sandbox_option__new_id(mock_module, vellum_client, base_command):
-    # GIVEN a single workflow configured
-    temp_dir = mock_module.temp_dir
-    module = mock_module.module
-    set_pyproject_toml = mock_module.set_pyproject_toml
-
-    # AND the pyproject.toml is empty
-    set_pyproject_toml({})
-
-    # AND a workflow exists in the module successfully
-    workflow_py_file_content = _ensure_workflow_py(temp_dir, module)
-
-    # AND the push API call would return successfully
-    new_workflow_sandbox_id = str(uuid4())
-    vellum_client.workflows.push.return_value = WorkflowPushResponse(
-        workflow_sandbox_id=new_workflow_sandbox_id,
-    )
-
-    # WHEN calling `vellum push` with the workflow sandbox option
-    runner = CliRunner()
-    result = runner.invoke(cli_main, base_command + [module, "--workflow-sandbox-id", new_workflow_sandbox_id])
-
-    # THEN it should succeed
-    assert result.exit_code == 0
-
-    # Get the last part of the module path and format it
-    expected_label = mock_module.module.split(".")[-1].replace("_", " ").title()
-    expected_artifact_name = f"{mock_module.module.replace('.', '__')}.tar.gz"
-
-    # AND we should have called the push API with the correct args
-    vellum_client.workflows.push.assert_called_once()
-    call_args = vellum_client.workflows.push.call_args.kwargs
-    assert json.loads(call_args["exec_config"])["workflow_raw_data"]["definition"]["name"] == "ExampleWorkflow"
-    assert call_args["label"] == expected_label
-    assert call_args["workflow_sandbox_id"] == new_workflow_sandbox_id
-    assert call_args["artifact"].name == expected_artifact_name
-    assert "deplyment_config" not in call_args
-
-    extracted_files = _extract_tar_gz(call_args["artifact"].read())
-    assert extracted_files["workflow.py"] == workflow_py_file_content
-
-
 @pytest.mark.parametrize(
     "base_command",
     [
