3da242c0edbde0c76351a2915271b2902119d835
Vincent <0426vincent@gmail.com>
2025-02-03 20:28:16 -0800

    Add test for no graph workflow

---
+++ tests/workflows/no_graph/workflow.py
@@ -0,0 +1,7 @@
+from vellum.workflows import BaseWorkflow
+from vellum.workflows.inputs.base import BaseInputs
+from vellum.workflows.state.base import BaseState
+
+
+class BasicNoGraphWorkflow(BaseWorkflow[BaseInputs, BaseState]):
+    pass
