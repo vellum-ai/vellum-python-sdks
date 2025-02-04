a13773805bd0357d2ff1c525d78e4d82fffabe39
Vincent <0426vincent@gmail.com>
2025-02-04 09:43:27 -0800

    Remove base import

--- tests/workflows/no_graph/workflow.py
+++ tests/workflows/no_graph/workflow.py
@@ -1,7 +1,5 @@
 from vellum.workflows import BaseWorkflow
-from vellum.workflows.inputs.base import BaseInputs
-from vellum.workflows.state.base import BaseState
 
 
-class BasicNoGraphWorkflow(BaseWorkflow[BaseInputs, BaseState]):
+class BasicNoGraphWorkflow(BaseWorkflow):
     pass
