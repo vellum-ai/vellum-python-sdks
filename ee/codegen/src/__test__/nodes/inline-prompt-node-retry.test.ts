34122de38b05c08a879b3ee52db4c59a84bf355a
Vincent <vincent@vellum.ai>
2025-03-05 09:27:35 -0800

    Reduce retry prompt test

--- ee/codegen/src/__test__/nodes/inline-prompt-node-retry.test.ts
+++ ee/codegen/src/__test__/nodes/inline-prompt-node-retry.test.ts
@@ -12,6 +12,7 @@ import { InlinePromptNode } from "src/generators/nodes/inline-prompt-node";
 describe("InlinePromptRetryNode", () => {
   let workflowContext: WorkflowContext;
   let writer: Writer;
+  let node: InlinePromptNode;
 
   beforeEach(() => {
     workflowContext = workflowContextFactory();
@@ -29,216 +30,203 @@ describe("InlinePromptRetryNode", () => {
     );
   });
 
-  const promptInputBlockTypes = [
-    "JINJA",
-    "CHAT_MESSAGE",
-    "FUNCTION_DEFINITION",
-    "VARIABLE",
-    "RICH_TEXT",
-  ] as const;
+  describe("basic", () => {
+    beforeEach(async () => {
+      const nodeData = inlinePromptNodeDataInlineVariantFactory({
+        blockType: "JINJA",
+        adornments: [
+          {
+            id: uuidv4(),
+            label: "RetryNodeLabel",
+            base: {
+              name: "RetryNode",
+              module: [
+                "vellum",
+                "workflows",
+                "nodes",
+                "core",
+                "retry_node",
+                "node",
+              ],
+            },
+            attributes: [
+              {
+                id: uuidv4(),
+                name: "max_attempts",
+                value: {
+                  type: "CONSTANT_VALUE",
+                  value: {
+                    type: "NUMBER",
+                    value: 3,
+                  },
+                },
+              },
+            ],
+          },
+        ],
+      });
 
-  describe.each(promptInputBlockTypes)("%s block type", (blockType) => {
+      const nodeContext = (await createNodeContext({
+        workflowContext,
+        nodeData,
+      })) as InlinePromptNodeContext;
+
+      node = new InlinePromptNode({
+        workflowContext,
+        nodeContext,
+      });
+    });
+
+    it(`getNodeFile`, async () => {
+      node.getNodeFile().write(writer);
+      expect(await writer.toStringFormatted()).toMatchSnapshot();
+    });
+    it(`getNodeDisplayFile`, async () => {
+      node.getNodeDisplayFile().write(writer);
+      expect(await writer.toStringFormatted()).toMatchSnapshot();
+    });
+  });
+  describe("basic retry adornment with delay", () => {
     let node: InlinePromptNode;
 
-    describe("basic", () => {
-      beforeEach(async () => {
-        const nodeData = inlinePromptNodeDataInlineVariantFactory({
-          blockType,
-          adornments: [
-            {
-              id: uuidv4(),
-              label: "RetryNodeLabel",
-              base: {
-                name: "RetryNode",
-                module: [
-                  "vellum",
-                  "workflows",
-                  "nodes",
-                  "core",
-                  "retry_node",
-                  "node",
-                ],
-              },
-              attributes: [
-                {
-                  id: uuidv4(),
-                  name: "max_attempts",
-                  value: {
-                    type: "CONSTANT_VALUE",
-                    value: {
-                      type: "NUMBER",
-                      value: 3,
-                    },
-                  },
-                },
+    beforeEach(async () => {
+      const nodeData = inlinePromptNodeDataInlineVariantFactory({
+        blockType: "JINJA",
+        adornments: [
+          {
+            id: uuidv4(),
+            label: "RetryNodeLabel",
+            base: {
+              name: "RetryNode",
+              module: [
+                "vellum",
+                "workflows",
+                "nodes",
+                "core",
+                "retry_node",
+                "node",
               ],
             },
-          ],
-        });
-
-        const nodeContext = (await createNodeContext({
-          workflowContext,
-          nodeData,
-        })) as InlinePromptNodeContext;
-
-        node = new InlinePromptNode({
-          workflowContext,
-          nodeContext,
-        });
-      });
-
-      it(`getNodeFile for ${blockType} block type`, async () => {
-        node.getNodeFile().write(writer);
-        expect(await writer.toStringFormatted()).toMatchSnapshot();
-      });
-      it(`getNodeDisplayFile for ${blockType} block type`, async () => {
-        node.getNodeDisplayFile().write(writer);
-        expect(await writer.toStringFormatted()).toMatchSnapshot();
-      });
-    });
-    describe("basic retry adornment with delay", () => {
-      let node: InlinePromptNode;
-
-      beforeEach(async () => {
-        const nodeData = inlinePromptNodeDataInlineVariantFactory({
-          blockType: blockType,
-          adornments: [
-            {
-              id: uuidv4(),
-              label: "RetryNodeLabel",
-              base: {
-                name: "RetryNode",
-                module: [
-                  "vellum",
-                  "workflows",
-                  "nodes",
-                  "core",
-                  "retry_node",
-                  "node",
-                ],
+            attributes: [
+              {
+                id: uuidv4(),
+                name: "max_attempts",
+                value: {
+                  type: "CONSTANT_VALUE",
+                  value: {
+                    type: "NUMBER",
+                    value: 3,
+                  },
+                },
               },
-              attributes: [
-                {
-                  id: uuidv4(),
-                  name: "max_attempts",
+              {
+                id: uuidv4(),
+                name: "delay",
+                value: {
+                  type: "CONSTANT_VALUE",
                   value: {
-                    type: "CONSTANT_VALUE",
-                    value: {
-                      type: "NUMBER",
-                      value: 3,
-                    },
+                    type: "NUMBER",
+                    value: 2,
                   },
                 },
-                {
-                  id: uuidv4(),
-                  name: "delay",
-                  value: {
-                    type: "CONSTANT_VALUE",
-                    value: {
-                      type: "NUMBER",
-                      value: 2,
-                    },
-                  },
-                },
-              ],
-            },
-          ],
-        });
-
-        const nodeContext = (await createNodeContext({
-          workflowContext,
-          nodeData,
-        })) as InlinePromptNodeContext;
-
-        node = new InlinePromptNode({
-          workflowContext,
-          nodeContext,
-        });
+              },
+            ],
+          },
+        ],
       });
 
-      it("getNodeFile", async () => {
-        node.getNodeFile().write(writer);
-        expect(await writer.toStringFormatted()).toMatchSnapshot();
-      });
+      const nodeContext = (await createNodeContext({
+        workflowContext,
+        nodeData,
+      })) as InlinePromptNodeContext;
 
-      it("getNodeDisplayFile", async () => {
-        node.getNodeDisplayFile().write(writer);
-        expect(await writer.toStringFormatted()).toMatchSnapshot();
+      node = new InlinePromptNode({
+        workflowContext,
+        nodeContext,
       });
     });
 
-    describe("basic retry adornment and try adornment", () => {
-      let node: InlinePromptNode;
-      const ERROR_OUTPUT_ID = "e7a1fbea-f5a7-4b31-a9ff-0d26c3de021f";
+    it("getNodeFile", async () => {
+      node.getNodeFile().write(writer);
+      expect(await writer.toStringFormatted()).toMatchSnapshot();
+    });
 
-      beforeEach(async () => {
-        // Create a prompt node with both errorOutputId and retry adornment
-        const nodeData = inlinePromptNodeDataInlineVariantFactory({
-          blockType: blockType,
-          errorOutputId: ERROR_OUTPUT_ID,
-          adornments: [
-            {
-              id: uuidv4(),
-              label: "RetryNodeLabel",
-              base: {
-                name: "RetryNode",
-                module: [
-                  "vellum",
-                  "workflows",
-                  "nodes",
-                  "core",
-                  "retry_node",
-                  "node",
-                ],
-              },
-              attributes: [
-                {
-                  id: uuidv4(),
-                  name: "max_attempts",
-                  value: {
-                    type: "CONSTANT_VALUE",
-                    value: {
-                      type: "NUMBER",
-                      value: 3,
-                    },
-                  },
-                },
-                {
-                  id: uuidv4(),
-                  name: "delay",
-                  value: {
-                    type: "CONSTANT_VALUE",
-                    value: {
-                      type: "NUMBER",
-                      value: 2,
-                    },
-                  },
-                },
+    it("getNodeDisplayFile", async () => {
+      node.getNodeDisplayFile().write(writer);
+      expect(await writer.toStringFormatted()).toMatchSnapshot();
+    });
+  });
+
+  describe("basic retry adornment and try adornment", () => {
+    let node: InlinePromptNode;
+    const ERROR_OUTPUT_ID = "e7a1fbea-f5a7-4b31-a9ff-0d26c3de021f";
+
+    beforeEach(async () => {
+      const nodeData = inlinePromptNodeDataInlineVariantFactory({
+        blockType: "JINJA",
+        errorOutputId: ERROR_OUTPUT_ID,
+        adornments: [
+          {
+            id: uuidv4(),
+            label: "RetryNodeLabel",
+            base: {
+              name: "RetryNode",
+              module: [
+                "vellum",
+                "workflows",
+                "nodes",
+                "core",
+                "retry_node",
+                "node",
               ],
             },
-          ],
-        });
-
-        const nodeContext = (await createNodeContext({
-          workflowContext,
-          nodeData,
-        })) as InlinePromptNodeContext;
-
-        node = new InlinePromptNode({
-          workflowContext,
-          nodeContext,
-        });
+            attributes: [
+              {
+                id: uuidv4(),
+                name: "max_attempts",
+                value: {
+                  type: "CONSTANT_VALUE",
+                  value: {
+                    type: "NUMBER",
+                    value: 3,
+                  },
+                },
+              },
+              {
+                id: uuidv4(),
+                name: "delay",
+                value: {
+                  type: "CONSTANT_VALUE",
+                  value: {
+                    type: "NUMBER",
+                    value: 2,
+                  },
+                },
+              },
+            ],
+          },
+        ],
       });
 
-      it("getNodeFile", async () => {
-        node.getNodeFile().write(writer);
-        expect(await writer.toStringFormatted()).toMatchSnapshot();
-      });
+      const nodeContext = (await createNodeContext({
+        workflowContext,
+        nodeData,
+      })) as InlinePromptNodeContext;
 
-      it("getNodeDisplayFile", async () => {
-        node.getNodeDisplayFile().write(writer);
-        expect(await writer.toStringFormatted()).toMatchSnapshot();
+      node = new InlinePromptNode({
+        workflowContext,
+        nodeContext,
       });
+    });
+
+    it("getNodeFile", async () => {
+      node.getNodeFile().write(writer);
+      expect(await writer.toStringFormatted()).toMatchSnapshot();
+    });
+
+    it("getNodeDisplayFile", async () => {
+      node.getNodeDisplayFile().write(writer);
+      expect(await writer.toStringFormatted()).toMatchSnapshot();
     });
   });
 });
