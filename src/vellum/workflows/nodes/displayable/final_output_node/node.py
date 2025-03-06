029a022d2988bd174d52b76e383ac19268269bf8
David Vargas <dvargas92495@gmail.com>
2025-03-06 07:39:02 -0500

    valid test failure fix

--- src/vellum/workflows/nodes/displayable/final_output_node/node.py
+++ src/vellum/workflows/nodes/displayable/final_output_node/node.py
@@ -1,7 +1,6 @@
 from typing import Any, Dict, Generic, Tuple, Type, TypeVar, get_args
 
-from vellum.workflows.errors.types import WorkflowErrorCode
-from vellum.workflows.exceptions import NodeException
+from vellum.workflows.constants import undefined
 from vellum.workflows.nodes.bases import BaseNode
 from vellum.workflows.nodes.bases.base import BaseNodeMeta
 from vellum.workflows.nodes.utils import cast_to_output_type
@@ -51,14 +50,14 @@ class FinalOutputNode(BaseNode[StateType], Generic[StateType, _OutputType], meta
     class Outputs(BaseNode.Outputs):
         # We use our mypy plugin to override the _OutputType with the actual output type
         # for downstream references to this output.
-        value: _OutputType  # type: ignore[valid-type]
+        value: _OutputType = undefined  # type: ignore[valid-type]
 
     def run(self) -> Outputs:
-        output_reference = self.Outputs.value
-        if not output_reference.instance:
-            raise NodeException(
-                code=WorkflowErrorCode.INVALID_OUTPUTS,
-                message=f"Must set the `value` Output on '{self.__class__.__name__}'.",
-            )
+        original_outputs = self.Outputs()
 
-        return self.Outputs(value=cast_to_output_type(output_reference.instance, self.__class__.get_output_type()))
+        return self.Outputs(
+            value=cast_to_output_type(
+                original_outputs.value,
+                self.__class__.get_output_type(),
+            )
+        )
