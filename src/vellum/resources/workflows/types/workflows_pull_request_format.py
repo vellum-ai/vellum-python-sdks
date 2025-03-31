5b418d4d50673bb78c326aab20ff39c40c17bb4f
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-03-31 03:45:29 +0000

    SDK regeneration

--- src/vellum/resources/workflows/types/workflows_pull_request_format.py
+++
@@ -1,3 +0,0 @@
-# WARNING: This file will be removed in a future release. Please import from "vellum.client" instead.
-
-from vellum.client.resources.workflows.types.workflows_pull_request_format import *
