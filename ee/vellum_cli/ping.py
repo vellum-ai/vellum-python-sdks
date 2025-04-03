96e89d587315a6436f4df250c5a83dcffcb5ec44
V <vincent@vellum.ai>
2025-04-03 16:11:12 -0700

    Add error handling for ping (#1324)

--- ee/vellum_cli/ping.py
+++ ee/vellum_cli/ping.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 
+from vellum.client.core.api_error import ApiError
 from vellum.workflows.vellum_client import create_vellum_client
 from vellum_cli.logger import load_cli_logger
 
@@ -10,8 +11,13 @@ def ping_command():
 
     client = create_vellum_client()
 
-    workspace = client.workspaces.workspace_identity()
-    organization = client.organizations.organization_identity()
+    try:
+        workspace = client.workspaces.workspace_identity()
+        organization = client.organizations.organization_identity()
+    except ApiError:
+        raise Exception(
+            "The API we tried to ping returned an invalid response. Please make sure your `VELLUM_API_URL` environment variable is set correctly."  # noqa: E501
+        )
 
     logger.info(
         f"""\
