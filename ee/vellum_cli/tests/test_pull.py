620b7b58d957e73f3395a4713e9064640e41d1e3
V <vincent@vellum.ai>
2025-04-08 19:23:58 -0700

    Fix pull test not changing directory (#1349)

--- ee/vellum_cli/tests/test_pull.py
+++ ee/vellum_cli/tests/test_pull.py
@@ -1248,6 +1248,7 @@ def test_pull__workflow_deployment_with_name_and_id(vellum_client):
         ]
     )
 
+    os.chdir(temp_dir)
     result = runner.invoke(cli_main, ["workflows", "pull", "--workflow-deployment", deployment_name])
     assert result.exit_code == 0
     with open(vellum_lock_json) as f:
