172cd25ba6296963e9bce40ebde599bf6cec1f48
David Vargas <dvargas92495@gmail.com>
2025-04-28 22:55:26 -0400

    Migrate custom prompt node into SDK examples

---
+++ examples/workflows/custom_prompt_node/nodes/bot_response.py
@@ -0,0 +1,10 @@
+from classifier.nodes.be_happy import BeHappyPrompt
+from classifier.nodes.cheer_up import CheerUpPrompt
+from classifier.nodes.settle_down import SettleDownPrompt
+
+from vellum.workflows.nodes.displayable import FinalOutputNode
+
+
+class BotResponse(FinalOutputNode):
+    class Outputs(FinalOutputNode.Outputs):
+        value = BeHappyPrompt.Outputs.text.coalesce(CheerUpPrompt.Outputs.text).coalesce(SettleDownPrompt.Outputs.text)
