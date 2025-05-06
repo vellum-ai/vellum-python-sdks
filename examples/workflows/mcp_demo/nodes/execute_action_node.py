e8b56ae1e91587ec9989d813669e4d986b19f0ba
David Vargas <vargas@vellum.ai>
2025-05-05 22:03:05 -0400

    Add the MCP example (#1642)

---
+++ examples/workflows/mcp_demo/nodes/execute_action_node.py
@@ -0,0 +1,65 @@
+import asyncio
+import os
+
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+from vellum import ChatMessage, FunctionCallChatMessageContent, StringChatMessageContent
+from vellum.workflows.nodes import BaseNode
+
+from ..state import State
+from .my_prompt_node import MyPromptNode
+
+
+class ExecuteActionNode(BaseNode[State]):
+    action = MyPromptNode.Outputs.results[0]
+
+    class Outputs(BaseNode.Outputs):
+        action_result: str
+        thinking: str
+
+    def run(self) -> Outputs:
+        if self.action.type != "FUNCTION_CALL":
+            raise ValueError(f"Action is not a function call: {self.action}")
+
+        self.state.chat_history.append(
+            ChatMessage(
+                role="ASSISTANT",
+                content=FunctionCallChatMessageContent.model_validate(self.action.model_dump()),
+            )
+        )
+
+        server_params = StdioServerParameters(
+            command="/usr/local/bin/docker",
+            args=["run", "-i", "--rm", "-e", "GITHUB_PERSONAL_ACCESS_TOKEN", "ghcr.io/github/github-mcp-server"],
+            env={
+                "GITHUB_PERSONAL_ACCESS_TOKEN": os.environ["GITHUB_PERSONAL_ACCESS_TOKEN"],
+            },
+        )
+
+        async def run_stdio():
+            async with stdio_client(server_params) as stdio_transport:
+                stdio_stream, write_stream = stdio_transport
+                async with ClientSession(stdio_stream, write_stream) as session:
+                    await session.initialize()
+                    response = await session.call_tool(
+                        name=self.action.value.name,
+                        arguments=self.action.value.arguments,
+                    )
+
+            return response.content
+
+        action_results = asyncio.run(run_stdio())
+        compiled_action_result = "\n".join([res.text for res in action_results if res.type == "text"])
+        self.state.chat_history.append(
+            ChatMessage(
+                role="FUNCTION",
+                content=StringChatMessageContent(value=compiled_action_result),
+                source=self.action.value.id,
+            )
+        )
+
+        return self.Outputs(
+            action_result=compiled_action_result,
+            thinking=f"Executed tool: {self.action.value.name}",
+        )
---
+++ examples/workflows/mcp_demo/nodes/mcp_client_node.py
@@ -0,0 +1,47 @@
+import asyncio
+import os
+from typing import List
+
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+from vellum import FunctionDefinition
+from vellum.workflows.nodes import BaseNode
+
+
+class MCPClientNode(BaseNode):
+    class Outputs(BaseNode.Outputs):
+        tools: List[FunctionDefinition]
+        thinking: str
+
+    def run(self) -> Outputs:
+        server_params = StdioServerParameters(
+            command="/usr/local/bin/docker",
+            args=["run", "-i", "--rm", "-e", "GITHUB_PERSONAL_ACCESS_TOKEN", "ghcr.io/github/github-mcp-server"],
+            env={
+                "GITHUB_PERSONAL_ACCESS_TOKEN": os.environ["GITHUB_PERSONAL_ACCESS_TOKEN"],
+            },
+        )
+
+        async def run_stdio():
+            async with stdio_client(server_params) as stdio_transport:
+                stdio_stream, write_stream = stdio_transport
+                async with ClientSession(stdio_stream, write_stream) as session:
+                    await session.initialize()
+                    response = await session.list_tools()
+
+            return response.tools
+
+        mcp_tools = asyncio.run(run_stdio())
+
+        return self.Outputs(
+            tools=[
+                FunctionDefinition(
+                    name=tool.name,
+                    description=tool.description,
+                    parameters=tool.inputSchema,
+                )
+                for tool in mcp_tools
+            ],
+            thinking=f"Retrieved {len(mcp_tools)} tools from MCP Server...",
+        )
