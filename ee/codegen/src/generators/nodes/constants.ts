2f6ba26bf4979364fa3612d35895d9be556689e5
Vincent <vincent@vellum.ai>
2025-05-10 16:18:53 -0700

    Remove name from node attr

--- ee/codegen/src/generators/nodes/constants.ts
+++ ee/codegen/src/generators/nodes/constants.ts
@@ -7,7 +7,6 @@ export enum AttributeType {
 export interface AttributeConfig {
   defaultValue: unknown;
   type?: AttributeType;
-  name: string;
 }
 
 export const NODE_ATTRIBUTES: Record<
@@ -17,28 +16,28 @@ export const NODE_ATTRIBUTES: Record<
   // From src/vellum/workflows/nodes/core/retry_node/node.py
   RetryNode: {
     retry_on_error_code: {
-      name: "retry_on_error_code",
       defaultValue: null,
       type: AttributeType.WorkflowErrorCode,
     },
-    retry_on_condition: { name: "retry_on_condition", defaultValue: null },
-    delay: { name: "delay", defaultValue: null },
+    retry_on_condition: {
+      defaultValue: null,
+    },
+    delay: {
+      defaultValue: null,
+    },
   },
   TryNode: {
     on_error_code: {
-      name: "on_error_code",
       defaultValue: null,
       type: AttributeType.WorkflowErrorCode,
     },
   },
   ToolCallingNode: {
     blocks: {
-      name: "blocks",
       defaultValue: null,
       type: AttributeType.PromptBlocks,
     },
     functions: {
-      name: "functions",
       defaultValue: null,
       type: AttributeType.Functions,
     },
