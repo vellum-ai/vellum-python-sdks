cd657a2e46159a28600783d5ad13c833145a6a76
Harrison Ngo <harrison@vellum.ai>
2025-05-16 16:06:28 -0400

    Handle lazy references of adorned nodes

--- tests/workflows/stream_retry_node_annotation/tests/test_workflow.py
+++ tests/workflows/stream_retry_node_annotation/tests/test_workflow.py
@@ -142,7 +142,7 @@ def test_workflow_stream__happy_path():
 
     # workflow snapshotted events
     workflow_snapshotted_events = [e for e in events if e.name == "workflow.execution.snapshotted"]
-    assert len(workflow_snapshotted_events) == 2
+    assert len(workflow_snapshotted_events) == 3
 
     # AND the total number of events is correct
-    assert len(events) == 21
+    assert len(events) == 22
