2f59ca2392527310e6367466044a15970fb12525
Vincent <vincent@vellum.ai>
2025-06-04 12:29:23 -0700

    CI

--- ee/codegen/src/generators/nodes/bases/single-file-base.ts
+++ ee/codegen/src/generators/nodes/bases/single-file-base.ts
@@ -1,9 +1,6 @@
-import { AstNode } from "@fern-api/python-ast/core/AstNode";
-
 import { BaseNode } from "./base";
 
 import { BaseNodeContext } from "src/context/node-context/base";
-import { BasePersistedFile } from "src/generators/base-persisted-file";
 import { WorkflowDataNode } from "src/types/vellum";
 
 export abstract class BaseSingleFileNode<
