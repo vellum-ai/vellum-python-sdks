c6c9e21eed3f1cf65ccfdf6bfe82db90f8389f7d
V <vincent@vellum.ai>
2025-06-11 18:17:20 -0700

    Fix output unique class (#1811)
    * Fix output unique class
    
    * Move up a level for getting unique class

--- ee/codegen/src/context/workflow-context/workflow-context.ts
+++ ee/codegen/src/context/workflow-context/workflow-context.ts
@@ -526,7 +526,7 @@ export class WorkflowContext {
     return this.classNames.has(className);
   }
 
-  private addUsedClassName(className: string): void {
+  public addUsedClassName(className: string): void {
     this.classNames.add(className);
   }
 
@@ -535,7 +535,6 @@ export class WorkflowContext {
     let numRenameAttempts = 0;
 
     if (!this.isClassNameUsed(sanitizedName)) {
-      this.addUsedClassName(sanitizedName);
       return sanitizedName;
     }
 
@@ -544,7 +543,6 @@ export class WorkflowContext {
       sanitizedName = `${createPythonClassName(baseName)}${numRenameAttempts}`;
     }
 
-    this.addUsedClassName(sanitizedName);
     return sanitizedName;
   }
 
