debcd86ef84940a588440572393e7585b66188c1
David Vargas <dvargas92495@gmail.com>
2025-06-25 09:36:20 -0400

    fix log level for cli tests

--- conftest.py
+++ conftest.py
@@ -17,10 +17,6 @@ from vellum.workflows.logging import load_logger
 
 def pytest_collection_modifyitems(session, config, items):
     """Set log level based on number of tests being run and their types"""
-    cli_tests_present = any("ee/vellum_cli/tests/" in str(item.fspath) for item in items)
-    if cli_tests_present:
-        return
-
     if len(items) == 1:
         os.environ["_PYTEST_SINGLE_TEST"] = "1"
     else:
@@ -31,13 +27,11 @@ def pytest_collection_modifyitems(session, config, items):
 def configure_logging() -> Generator[None, None, None]:
     """Used to output logs when running tests"""
 
-    cli_tests_present = any("ee/vellum_cli/tests/" in str(arg) for arg in sys.argv)
-
     env_vars = dotenv_values()
     dotenv_log_level = env_vars.get("LOG_LEVEL")
     if dotenv_log_level and not os.environ.get("LOG_LEVEL"):
         os.environ["LOG_LEVEL"] = dotenv_log_level
-    elif not os.environ.get("LOG_LEVEL") and not cli_tests_present:
+    elif not os.environ.get("LOG_LEVEL"):
         is_single_test = os.environ.get("_PYTEST_SINGLE_TEST") == "1"
         if is_single_test:
             os.environ["LOG_LEVEL"] = "DEBUG"
