45fa4ba3768612e0918a492fc3b25a4dbf1681e8
David Vargas <dvargas92495@gmail.com>
2025-07-09 17:11:59 -0400

    setup test properly

--- tests/workflows/state_array_streaming/workflow.py
+++ tests/workflows/state_array_streaming/workflow.py
@@ -18,14 +18,12 @@ class State(BaseState):
 class StateWritingNode(BaseNode[State]):
     message = Inputs.message
 
-    class Outputs(BaseOutputs):
-        final_messages: List[str]
-
     def run(self) -> Iterator[BaseOutput]:
         for i in range(3):
             new_message = f"{self.message} - write {i + 1}"
             self.state.messages.append(new_message)
-            yield BaseOutput(name="final_messages", delta=new_message)
+
+        return self.Outputs()
 
 
 class StateArrayStreamingWorkflow(BaseWorkflow[Inputs, State]):
