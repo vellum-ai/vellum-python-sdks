06e5314ca438f8199d16de41fa0d9bd3fc9c8cc1
Harrison Ngo <harrison@vellum.ai>
2025-08-01 17:32:31 -0400

    npm run lint

--- ee/codegen/src/utils/typing.ts
+++ ee/codegen/src/utils/typing.ts
@@ -2,7 +2,7 @@ import { AstNode } from "@fern-api/python-ast/core/AstNode";
 import { isNil } from "lodash";
 
 import { ValueGenerationError } from "src/generators/errors";
-import { InlinePromptNode, WorkflowDataNode } from "src/types/vellum";
+import { WorkflowDataNode } from "src/types/vellum";
 
 export function isDefined<TValue>(value: TValue | undefined): value is TValue {
   return value !== undefined;
