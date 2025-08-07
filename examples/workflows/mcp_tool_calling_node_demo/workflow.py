71544f5e83334fdfdbba2c9470035d56d4ecc2ca
Vincent <vincent@vellum.ai>
2025-08-07 18:01:49 -0400

    MCP demo with tool calling node

--- examples/workflows/mcp_tool_calling_node_demo/workflow.py
+++ examples/workflows/mcp_tool_calling_node_demo/workflow.py
@@ -1,25 +1,22 @@
 from vellum.workflows import BaseWorkflow
+from vellum.workflows.state.base import BaseState
 
 from .inputs import Inputs
-from .nodes.execute_action_node import ExecuteActionNode
-from .nodes.exit_node import ExitNode
-from .nodes.mcp_client_node import MCPClientNode
 from .nodes.my_prompt_node import MyPromptNode
-from .state import State
 
 
-class MCPDemoWorkflow(BaseWorkflow[Inputs, State]):
+class MCPDemoWorkflow(BaseWorkflow[Inputs, BaseState]):
     """
-    An example workflow that acts as an MCP Client. It's currently hardcoded to interact with the
-    Github MCP Server, found here: https://github.com/github/github-mcp-server. To run this demo,
+    An example workflow that uses the built-in ToolCallingNode with MCP Server.
+    It interacts with the Github MCP Server to manage the user's GitHub account.
+
+    To run this demo:
     - Create a Github Access Token and export it as the environment variable `GITHUB_PERSONAL_ACCESS_TOKEN`
-    - Run this workflow: `python -m examples.mcp_without_sdk.chat`
+    - Run this workflow: `python -m examples.workflows.mcp_tool_calling_node_demo.chat`
     """
 
-    graph = MCPClientNode >> {
-        MyPromptNode.Ports.action >> ExecuteActionNode >> MCPClientNode,
-        MyPromptNode.Ports.exit >> ExitNode,
-    }
+    graph = MyPromptNode
 
     class Outputs:
-        answer = ExitNode.Outputs.value
+        text = MyPromptNode.Outputs.text
+        chat_history = MyPromptNode.Outputs.chat_history
