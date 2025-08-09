09ecff156a94bdeb86ba1ea4abe09808b0d73591
V <vincent@vellum.ai>
2025-08-09 18:17:33 -0400

    MCP demo with tool calling node (#2244)

--- examples/workflows/mcp_tool_calling_node_demo/nodes/exit_node.py
+++
@@ -1,8 +0,0 @@
-from vellum.workflows.nodes.displayable.final_output_node.node import FinalOutputNode
-
-from .my_prompt_node import MyPromptNode
-
-
-class ExitNode(FinalOutputNode):
-    class Outputs(FinalOutputNode.Outputs):
-        value = MyPromptNode.Outputs.text
