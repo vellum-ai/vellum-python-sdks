8cd8d93d67c4cb8f6fe5d6c0d37619bcf5a6af9d
Vincent <vincent@vellum.ai>
2025-08-20 22:35:25 -0700

    Fix mypy

--- src/vellum/workflows/resolvers/resolver.py
+++ src/vellum/workflows/resolvers/resolver.py
@@ -11,21 +11,25 @@ logger = logging.getLogger(__name__)
 
 class VellumResolver(BaseWorkflowResolver):
     def get_latest_execution_events(self) -> Iterator[WorkflowEvent]:
-        pass
+        return iter([])
 
     def get_state_snapshot_history(self) -> Iterator[BaseState]:
-        pass
+        return iter([])
 
     def load_state(self, previous_execution_id: Optional[UUID] = None) -> BaseState:
         if previous_execution_id is None:
-            return None
+            return BaseState()
 
         if not self._context:
             logger.warning("Cannot load state: No workflow context registered")
-            return None
+            return BaseState()
 
         client = self._context.vellum_client
         response = client.workflow_executions.retrieve_workflow_execution_detail(
-            execution_id=previous_execution_id,
+            execution_id=str(previous_execution_id),
         )
-        return response.state
+
+        if response.state:
+            return BaseState(**response.state)
+        else:
+            return BaseState()
