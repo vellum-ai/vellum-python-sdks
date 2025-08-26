c2167d73a3713fcb5dbd043e010ba4b334108a9b
Vincent <vincent@vellum.ai>
2025-08-26 14:44:29 -0700

    Fix test

--- tests/workflows/span_link_workflow/workflow.py
+++ tests/workflows/span_link_workflow/workflow.py
@@ -6,22 +6,30 @@ from vellum.workflows.resolvers.resolver import VellumResolver
 from vellum.workflows.state.base import BaseState
 
 
+class Inputs(BaseInputs):
+    user_message: str
+
+
 class State(BaseState):
     execution_count: int = 0
 
 
 class CounterNode(BaseNode[State]):
+    user_message = Inputs.user_message
+
     class Outputs(BaseOutputs):
         current_count: int
+        user_message: str
 
     def run(self) -> Outputs:
         self.state.execution_count += 1
-        return self.Outputs(current_count=self.state.execution_count)
+        return self.Outputs(current_count=self.state.execution_count, user_message=self.user_message)
 
 
-class SpanLinkWorkflow(BaseWorkflow[BaseInputs, State]):
+class SpanLinkWorkflow(BaseWorkflow[Inputs, State]):
     graph = CounterNode
     resolvers = [VellumResolver()]
 
     class Outputs(BaseOutputs):
         current_count = CounterNode.Outputs.current_count
+        user_message = CounterNode.Outputs.user_message
