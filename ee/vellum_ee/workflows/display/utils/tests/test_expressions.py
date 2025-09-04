e74b907be0d2f61b44ed5b65a14c6591481ac4b4
Alex Nork <alexnork@gmail.com>
2025-09-04 17:04:37 -0400

    parametrize test

--- ee/vellum_ee/workflows/display/utils/tests/test_expressions.py
+++ ee/vellum_ee/workflows/display/utils/tests/test_expressions.py
@@ -1,5 +1,7 @@
 """Tests for expressions serialization utilities"""
 
+import pytest
+
 from vellum.workflows.constants import undefined
 from vellum_ee.workflows.display.utils.expressions import serialize_value
 
@@ -7,28 +9,21 @@ from vellum_ee.workflows.display.utils.expressions import serialize_value
 class TestSerializeValue:
     """Tests for the serialize_value function"""
 
-    def test_serialize_undefined_value(self):
-        """Test that undefined values are properly serialized to JSON null
+    @pytest.mark.parametrize(
+        "value, expected_serialization",
+        [
+            (undefined, {"type": "CONSTANT_VALUE", "value": {"type": "JSON", "value": None}}),
+            (None, {"type": "CONSTANT_VALUE", "value": {"type": "JSON", "value": None}}),
+        ],
+    )
+    def test_serialize_value(self):
+        """Test that values are properly serialized to JSON null
 
         This test prevents regression of APO-1404 where undefined values
         would cause a "'undefined' object cannot be converted to SchemaSerializer" error
         """
         # WHEN we serialize an undefined value
         result = serialize_value(display_context=None, value=undefined)
-
-        # THEN it should be serialized as a constant JSON null value
-        assert result == {
-            "type": "CONSTANT_VALUE",
-            "value": {
-                "type": "JSON",
-                "value": None,
-            },
-        }
-
-    def test_serialize_none_value(self):
-        """Test that None values are handled differently from undefined"""
-        # WHEN we serialize a None value
-        result = serialize_value(display_context=None, value=None)
 
         # THEN it should be serialized as a constant JSON null value
         assert result == {
