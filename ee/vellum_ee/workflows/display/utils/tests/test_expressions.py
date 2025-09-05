c826d6f0e6efebd35f4f1bb19d8946ecfba6bdd6
Alex Nork <alexnork@gmail.com>
2025-09-04 21:31:21 -0400

    remove unused param

--- ee/vellum_ee/workflows/display/utils/tests/test_expressions.py
+++ ee/vellum_ee/workflows/display/utils/tests/test_expressions.py
@@ -9,27 +9,10 @@ from vellum_ee.workflows.display.utils.expressions import serialize_value
 class TestSerializeValue:
     """Tests for the serialize_value function"""
 
-    @pytest.mark.parametrize(
-        "value, expected_serialization",
-        [
-            (undefined, {"type": "CONSTANT_VALUE", "value": {"type": "JSON", "value": None}}),
-            (None, {"type": "CONSTANT_VALUE", "value": {"type": "JSON", "value": None}}),
-        ],
-    )
-    def test_serialize_value(self):
-        """Test that values are properly serialized to JSON null
-
-        This test prevents regression of APO-1404 where undefined values
-        would cause a "'undefined' object cannot be converted to SchemaSerializer" error
-        """
-        # WHEN we serialize an undefined value
-        result = serialize_value(display_context=None, value=undefined)
-
-        # THEN it should be serialized as a constant JSON null value
-        assert result == {
-            "type": "CONSTANT_VALUE",
-            "value": {
-                "type": "JSON",
-                "value": None,
-            },
-        }
+    @pytest.mark.parametrize("value", [undefined, None])
+    def test_serialize_value(self, value):
+        """Test that values are properly serialized"""
+        # WHEN we serialize a value
+        result = serialize_value(display_context=None, value=value)
+        # THEN it should match the expected serialization
+        assert result == {"type": "CONSTANT_VALUE", "value": {"type": "JSON", "value": None}}
