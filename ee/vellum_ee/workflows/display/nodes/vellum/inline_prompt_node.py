c5312b78783cad3b3b8c849cb318cffb97c31476
Alex Nork <alexnork@gmail.com>
2025-10-01 17:47:16 -0400

    revert one change

--- ee/vellum_ee/workflows/display/nodes/vellum/inline_prompt_node.py
+++ ee/vellum_ee/workflows/display/nodes/vellum/inline_prompt_node.py
@@ -5,14 +5,9 @@ from vellum import FunctionDefinition, PromptBlock, RichTextChildBlock, VellumVa
 from vellum.workflows.descriptors.base import BaseDescriptor
 from vellum.workflows.nodes import InlinePromptNode
 from vellum.workflows.types.core import JsonObject
-from vellum.workflows.types.definition import (
-    ComposioToolDefinition,
-    DeploymentDefinition,
-    VellumIntegrationToolDefinition,
-)
+from vellum.workflows.types.definition import DeploymentDefinition, VellumIntegrationToolDefinition
 from vellum.workflows.types.generics import is_workflow_class
 from vellum.workflows.utils.functions import (
-    compile_composio_tool_definition,
     compile_function_definition,
     compile_inline_workflow_function_definition,
     compile_vellum_integration_tool_definition,
@@ -172,8 +167,6 @@ class BaseInlinePromptNodeDisplay(BaseNodeDisplay[_InlinePromptNodeType], Generi
             normalized_functions = compile_workflow_deployment_function_definition(function, display_context.client)
         elif isinstance(function, VellumIntegrationToolDefinition):
             normalized_functions = compile_vellum_integration_tool_definition(function, display_context.client)
-        elif isinstance(function, ComposioToolDefinition):
-            normalized_functions = compile_composio_tool_definition(function)
         else:
             raise ValueError(f"Unsupported function type: {type(function)}")
         return {
