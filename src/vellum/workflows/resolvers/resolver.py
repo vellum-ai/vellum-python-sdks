1304f8ae7ed6b72ef57e8047182f948317381e86
David Vargas <dvargas92495@gmail.com>
2025-11-11 00:14:50 -0500

    Fix vellum resolver ids

--- src/vellum/workflows/resolvers/resolver.py
+++ src/vellum/workflows/resolvers/resolver.py
@@ -78,8 +78,8 @@ class VellumResolver(BaseWorkflowResolver):
 
         return LoadStateResult(
             state=state,
-            previous_trace_id=response.previous_trace_id,
-            previous_span_id=response.previous_span_id,
+            previous_trace_id=response.trace_id,
+            previous_span_id=response.span_id,
             root_trace_id=response.root_trace_id,
             root_span_id=response.root_span_id,
         )
