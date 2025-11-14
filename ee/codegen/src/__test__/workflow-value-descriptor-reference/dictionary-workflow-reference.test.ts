49fa8f574cb8d0992c424101000a3ecd0bededc9
Vincent <vincent@vellum.ai>
2025-11-13 20:07:37 -0800

    Codegen dict ref definition

--- ee/codegen/src/__test__/workflow-value-descriptor-reference/dictionary-workflow-reference.test.ts
+++ ee/codegen/src/__test__/workflow-value-descriptor-reference/dictionary-workflow-reference.test.ts
@@ -2,6 +2,8 @@ import { Writer } from "@fern-api/python-ast/core/Writer";
 import { vi } from "vitest";
 
 import { workflowContextFactory } from "src/__test__/helpers";
+import { inputVariableContextFactory } from "src/__test__/helpers/input-variable-context-factory";
+import { stateVariableContextFactory } from "src/__test__/helpers/state-variable-context-factory";
 import { WorkflowContext } from "src/context";
 import { DictionaryWorkflowReference } from "src/generators/workflow-value-descriptor-reference/dictionary-workflow-reference";
 import { DictionaryWorkflowReference as DictionaryWorkflowReferenceType } from "src/types/vellum";
@@ -192,6 +194,97 @@ describe("DictionaryWorkflowReference", () => {
       nodeInputWorkflowReferencePointer: exprDictReference,
     });
 
+    const writer = new Writer();
+    reference.write(writer);
+
+    expect(await writer.toStringFormatted()).toMatchSnapshot();
+  });
+  it("should generate correct AST for dictionary with definition", async () => {
+    const definitionDictReference: DictionaryWorkflowReferenceType = {
+      type: "DICTIONARY_REFERENCE",
+      entries: [
+        {
+          id: "160c6ef1-30b4-40ff-8cad-88d73bc6ea54",
+          key: "chat_history",
+          value: {
+            type: "BINARY_EXPRESSION",
+            lhs: {
+              type: "WORKFLOW_STATE",
+              stateVariableId: "2ae688ad-8690-4765-a5fa-aecc7d6496e5",
+            },
+            operator: "+",
+            rhs: {
+              type: "DICTIONARY_REFERENCE",
+              entries: [
+                {
+                  id: "f50f80a5-3b36-4077-a90f-c13b38fd7919",
+                  key: "text",
+                  value: {
+                    type: "WORKFLOW_INPUT",
+                    inputVariableId: "cb4bd466-58e9-4ecd-a2b3-22ce280a0422",
+                  },
+                },
+                {
+                  id: "2047e859-8c1a-42b9-b4d4-b6d6fc7b33b2",
+                  key: "role",
+                  value: {
+                    type: "CONSTANT_VALUE",
+                    value: { type: "STRING", value: "USER" },
+                  },
+                },
+                {
+                  id: "ca0c0a31-e903-4908-998d-10916e287d77",
+                  key: "content",
+                  value: {
+                    type: "CONSTANT_VALUE",
+                    value: { type: "JSON", value: null },
+                  },
+                },
+                {
+                  id: "78565b95-23a0-4be4-a9ce-89893a60f458",
+                  key: "source",
+                  value: {
+                    type: "CONSTANT_VALUE",
+                    value: { type: "JSON", value: null },
+                  },
+                },
+              ],
+              definition: {
+                name: "ChatMessage",
+                module: ["vellum", "client", "types"],
+              },
+            },
+          },
+        },
+      ],
+    };
+
+    workflowContext.addStateVariableContext(
+      stateVariableContextFactory({
+        stateVariableData: {
+          id: "2ae688ad-8690-4765-a5fa-aecc7d6496e5",
+          key: "chat_history",
+          type: "CHAT_HISTORY",
+        },
+        workflowContext,
+      })
+    );
+
+    workflowContext.addInputVariableContext(
+      inputVariableContextFactory({
+        inputVariableData: {
+          id: "cb4bd466-58e9-4ecd-a2b3-22ce280a0422",
+          key: "text",
+          type: "STRING",
+        },
+        workflowContext,
+      })
+    );
+
+    const reference = new DictionaryWorkflowReference({
+      workflowContext,
+      nodeInputWorkflowReferencePointer: definitionDictReference,
+    });
     const writer = new Writer();
     reference.write(writer);
 
--- ee/codegen/src/generators/workflow-value-descriptor-reference/dictionary-workflow-reference.ts
+++ ee/codegen/src/generators/workflow-value-descriptor-reference/dictionary-workflow-reference.ts
@@ -10,11 +10,39 @@ export class DictionaryWorkflowReference extends BaseNodeInputWorkflowReference<
   getAstNode(): AstNode | undefined {
     const dictionaryReference = this.nodeInputWorkflowReferencePointer;
     const entries = dictionaryReference.entries;
+    const definition = dictionaryReference.definition;
 
     if (!entries || entries.length === 0) {
       return python.TypeInstantiation.dict([]);
     }
 
+    // If definition exists, generate class instantiation instead of dict
+    if (definition) {
+      const classArguments = entries.map((entry) => {
+        const valueNode = new WorkflowValueDescriptor({
+          nodeContext: this.nodeContext,
+          workflowContext: this.workflowContext,
+          workflowValueDescriptor: entry.value,
+          iterableConfig: this.iterableConfig,
+          attributeConfig: this.attributeConfig,
+        });
+
+        return python.methodArgument({
+          name: entry.key,
+          value: valueNode,
+        });
+      });
+
+      return python.instantiateClass({
+        classReference: python.reference({
+          name: definition.name,
+          modulePath: definition.module,
+        }),
+        arguments_: classArguments,
+      });
+    }
+
+    // Otherwise, generate dict as before
     const dictEntries = entries.map((entry) => {
       const keyAstNode = python.TypeInstantiation.str(entry.key);
 
