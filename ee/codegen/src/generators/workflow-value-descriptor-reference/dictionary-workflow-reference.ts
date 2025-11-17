e9a66db1e13da97bf9c4538e99194d418110349d
Vincent <vincent@vellum.ai>
2025-11-17 13:37:02 -0800

    Filter out none

--- ee/codegen/src/generators/workflow-value-descriptor-reference/dictionary-workflow-reference.ts
+++ ee/codegen/src/generators/workflow-value-descriptor-reference/dictionary-workflow-reference.ts
@@ -1,5 +1,6 @@
 import { python } from "@fern-api/python-ast";
 import { AstNode } from "@fern-api/python-ast/core/AstNode";
+import { isNil } from "lodash";
 
 import { BaseNodeInputWorkflowReference } from "./BaseNodeInputWorkflowReference";
 
@@ -18,20 +19,23 @@ export class DictionaryWorkflowReference extends BaseNodeInputWorkflowReference<
 
     // If definition exists, generate class instantiation instead of dict
     if (definition) {
-      const classArguments = entries.map((entry) => {
-        const valueNode = new WorkflowValueDescriptor({
-          nodeContext: this.nodeContext,
-          workflowContext: this.workflowContext,
-          workflowValueDescriptor: entry.value,
-          iterableConfig: this.iterableConfig,
-          attributeConfig: this.attributeConfig,
-        });
+      // Filter out entries with None values to avoid generating =None parameters
+      const classArguments = entries
+        .filter((entry) => !isConstantNullValue(entry.value))
+        .map((entry) => {
+          const valueNode = new WorkflowValueDescriptor({
+            nodeContext: this.nodeContext,
+            workflowContext: this.workflowContext,
+            workflowValueDescriptor: entry.value,
+            iterableConfig: this.iterableConfig,
+            attributeConfig: this.attributeConfig,
+          });
 
-        return python.methodArgument({
-          name: entry.key,
-          value: valueNode,
+          return python.methodArgument({
+            name: entry.key,
+            value: valueNode,
+          });
         });
-      });
 
       return python.instantiateClass({
         classReference: python.reference({
@@ -64,4 +68,14 @@ export class DictionaryWorkflowReference extends BaseNodeInputWorkflowReference<
       endWithComma: true,
     });
   }
+}
+
+function isConstantNullValue(
+  valueDescriptor: DictionaryWorkflowReferenceType["entries"][number]["value"]
+): boolean {
+  return (
+    valueDescriptor?.type === "CONSTANT_VALUE" &&
+    valueDescriptor.value?.type === "JSON" &&
+    isNil(valueDescriptor.value.value)
+  );
 }
