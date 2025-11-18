13795f9eb39b513a5ac5e06a9eb26ed81805e139
Noa Flaherty <noa@vellum.ai>
2025-11-17 22:11:17 -0500

    Add new util for uploading file to vellum

--- src/vellum/utils/files/__init__.py
+++ src/vellum/utils/files/__init__.py
@@ -2,10 +2,12 @@ from vellum.utils.files.exceptions import FileNotFoundError, FileRetrievalError,
 from vellum.utils.files.read import read_vellum_file
 from vellum.utils.files.stream import stream_vellum_file
 from vellum.utils.files.types import VellumFileTypes
+from vellum.utils.files.upload import upload_vellum_file
 
 __all__ = [
     "read_vellum_file",
     "stream_vellum_file",
+    "upload_vellum_file",
     "VellumFileTypes",
     "VellumFileError",
     "InvalidFileSourceError",
