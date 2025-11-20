32895bf84d1564523179266b618e9c6ce1586a94
Vincent <vincent@vellum.ai>
2025-11-20 11:13:39 -0800

    Fix inheritance

--- ee/vellum_ee/workflows/display/nodes/vellum/tool_prompt_node.py
+++ ee/vellum_ee/workflows/display/nodes/vellum/tool_prompt_node.py
@@ -2,12 +2,12 @@ from typing import Generic, TypeVar
 
 from vellum.workflows.nodes.displayable.tool_calling_node.utils import ToolPromptNode
 from vellum_ee.workflows.display.editor import NodeDisplayData
-from vellum_ee.workflows.display.nodes.base_node_display import BaseNodeDisplay
+from vellum_ee.workflows.display.nodes.vellum.inline_prompt_node import BaseInlinePromptNodeDisplay
 
 _ToolPromptNodeType = TypeVar("_ToolPromptNodeType", bound=ToolPromptNode)
 
 
-class BaseToolPromptNodeDisplay(BaseNodeDisplay[_ToolPromptNodeType], Generic[_ToolPromptNodeType]):
+class BaseToolPromptNodeDisplay(BaseInlinePromptNodeDisplay[_ToolPromptNodeType], Generic[_ToolPromptNodeType]):
     """
     Display class for ToolPromptNode that sets the icon and color to match PROMPT nodes.
     """
